4ae22ade063bd53ae3d8abfbec9bd318cf8e9da5
Henry Wong <henryw48@nycstudents.net>
2022-09-30 12:32:01 -0400

    added Rectangle and RectangleRunner classes

---
+++ src/RectangleRunner.java
@@ -0,0 +1,10 @@
+public class RectangleRunner {
+    public static void main(String[] args)
+    {
+        public void printArea() {
+            int area = calculateArea();
+            System.out.println("My area is " + area);
+    }
+
+    }
+}
